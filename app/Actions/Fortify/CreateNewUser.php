ac613ee3dd232cce2d2a931e549d7defba43cbe4
Faisal <faisalvin15@gmail.com>
2022-03-17 06:43:13 +0700

    Add new folder and file structur, and custom fortify,middleware and providers laravel

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -4,54 +4,67 @@ namespace App\Actions\Fortify;
 
 use App\Models\Team;
 use App\Models\User;
+use App\Models\DetailUser;
+
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
 use Laravel\Jetstream\Jetstream;
+use phpDocumentor\Reflection\Types\Null_;
 
 class CreateNewUser implements CreatesNewUsers
 {
-    use PasswordValidationRules;
+  use PasswordValidationRules;
 
-    /**
-     * Create a newly registered user.
-     *
-     * @param  array  $input
-     * @return \App\Models\User
-     */
-    public function create(array $input)
-    {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
-        ])->validate();
+  /**
+   * Create a newly registered user.
+   *
+   * @param  array  $input
+   * @return \App\Models\User
+   */
+  public function create(array $input)
+  {
+    Validator::make($input, [
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+      'password' => $this->passwordRules(),
+      'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
+    ])->validate();
 
-        return DB::transaction(function () use ($input) {
-            return tap(User::create([
-                'name' => $input['name'],
-                'email' => $input['email'],
-                'password' => Hash::make($input['password']),
-            ]), function (User $user) {
-                $this->createTeam($user);
-            });
-        });
-    }
+    return DB::transaction(function () use ($input) {
+      return tap(User::create([
+        'name' => $input['name'],
+        'email' => $input['email'],
+        'password' => Hash::make($input['password']),
+      ]), function (User $user) {
+        $this->createTeam($user);
 
-    /**
-     * Create a personal team for the user.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    protected function createTeam(User $user)
-    {
-        $user->ownedTeams()->save(Team::forceCreate([
-            'user_id' => $user->id,
-            'name' => explode(' ', $user->name, 2)[0]."'s Team",
-            'personal_team' => true,
-        ]));
-    }
+        // Add to detail user
+        $detail_user = new DetailUser;
+        $detail_user->users_id = $user->id;
+        $detail_user->photo = Null;
+        $detail_user->role = Null;
+        $detail_user->contact_number = Null;
+        $detail_user->biography = Null;
+        $detail_user->save();
+      });
+    });
+  }
+
+  /**
+   * Create a personal team for the user.
+   *
+   * @param  \App\Models\User  $user
+   * @return void
+   */
+  protected function createTeam(User $user)
+  {
+    $user->ownedTeams()->save(Team::forceCreate([
+      'user_id' => $user->id,
+      'name' => explode(' ', $user->name, 2)[0] . "'s Team",
+      'personal_team' => true,
+    ]));
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// front (Landing Page)
+use App\Http\Controllers\Landing\LandingController;
+
+// Member (Dashboard Page)
+use App\Http\Controllers\Dashboard\MemberController;
+use App\Http\Controllers\Dashboard\MyOrderController;
+use App\Http\Controllers\Dashboard\ProfileController;
+use App\Http\Controllers\Dashboard\RequestController;
+use App\Http\Controllers\Dashboard\ServiceController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +25,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//   return view('welcome');
+// });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//   return view('dashboard');
+// })->name('dashboard');
+
+
+// Membuar route default- yang bisa diakses sebelum login tanpa perlu authentication
+Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
+Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
+Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
+Route::get('explore', LandingController::class, 'explore')->name('explore.landing');
+Route::resource('/', LandingController::class);
+
+Route::group(
+  ['prefix' => 'member', 'as' => 'member.', 'middleware' => ['auth:sanctum', 'verified']],
+  function () {
+
+
+    //  Dashboard
+    Route::resource('dashboard', MemberController::class);
+
+    // Service
+    Route::resource('service', ServiceController::class);
+
+    // Request
+    Route::get('approve_request/{id}', RequestController::class, 'approve')->name('approve.request');
+    Route::resource('request', RequestController::class);
+
+    // MyOrder
+    Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accept.order');
+    Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('reject.order');
+    Route::resource('order', MyOrderController::class);
+
+    // Profile
+    Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.photo.profile');
+    Route::resource('profile', ProfileController::class);
+  }
+);
