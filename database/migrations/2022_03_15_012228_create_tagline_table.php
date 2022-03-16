09d44f6b093bf09d18006bb59ede97fe14d1574b
Faisal <faisalvin15@gmail.com>
2022-03-16 09:24:46 +0700

    Add foreign key migration table

--- database/migrations/2022_03_15_012228_create_tagline_table.php
+++ database/migrations/2022_03_15_012228_create_tagline_table.php
@@ -6,29 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTaglineTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('tagline', function (Blueprint $table) {
-            $table->id();
-            $table->integer('service_id')->nullable();
-            $table->string('tagline');
-            $table->softDeletes();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('tagline', function (Blueprint $table) {
+      $table->id();
+      // $table->integer('service_id')->nullable();
+      $table->foreignId('service_id')->nullable()->index('fk_tagline_to_service');
+      $table->string('tagline');
+      $table->softDeletes();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('tagline');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('tagline');
+  }
 }
--- database/migrations/2022_03_15_012242_create_advantage_service_table.php
+++ database/migrations/2022_03_15_012242_create_advantage_service_table.php
@@ -6,29 +6,30 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAdvantageServiceTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('advantage_service', function (Blueprint $table) {
-          $table->id();
-          $table->integer('service_id')->nullable();
-          $table->string('advantage');
-          $table->softDeletes();
-          $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('advantage_service', function (Blueprint $table) {
+      $table->id();
+      // $table->integer('service_id')->nullable();
+      $table->foreignId('service_id')->nullable()->index('fk_advantage_service_to_service');
+      $table->string('advantage');
+      $table->softDeletes();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('advantage_service');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('advantage_service');
+  }
 }
